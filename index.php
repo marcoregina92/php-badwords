0ed628000c54862f69d8415316c63bc6e9c3dfc2
Marco <marcoregina92@gmail.com>
2023-12-15 15:26:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php $nome = "Ciao Mondo" ?>
+    <h1><?php echo $nome . " esempio 1" ?></h1>
+    <p><?php echo "$nome esempio 2" ?></p>
+</body>
+</html>
